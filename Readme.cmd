7005e9c6ab77f7915d2d4de96c2e5ce84bb2b97b
a <amir_wise@hotmail.com>
2020-05-13 18:59:01 +0100

    test

---
+++ Readme.cmd
@@ -0,0 +1,14 @@
+## This project is to run the server on your local machine
+
+run the command
+
+npm install
+
+npm run start  init.json
+
+## Environment requirements
+
+# Node 13.8.0
+# Npm 6.13.6
+# Angular Cli 9.1.0
+# OS is 64 bit
